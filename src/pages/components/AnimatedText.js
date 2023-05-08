f57c44d4c29d846a8e66e9bf7952be9ffd90dcb0
vigilante23 <digvijay1999nagarkoti@gmail.com>
2023-05-09 01:39:45 +0530

    animated

--- src/pages/components/AnimatedText.js
+++ src/pages/components/AnimatedText.js
@@ -37,7 +37,7 @@ const AnimatedText = ({text, className=""}) => {
         <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className} `}
         variants={quote} initial='initial' animate='animate'> 
         {
-            text.split(" ").map((word, index) =>
+            text?.split(" ").map((word, index) =>
             <motion.span key= {word+'-'+index} className='inline-block '
             variants = {singleword} >
                 {word}&nbsp;
